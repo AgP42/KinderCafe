38f4b5e9979bddca4a6e7312246831b648be9962
Agathe Pinel <agp.com@mailbox.org>
2019-07-01 14:11:17 +0200

    addresse autocomplete and map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,18 @@ class PlacesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    #### to display the markers on the map for thoses with address
+    @match_places = Place.where.not(latitude: nil, longitude: nil)
+
+    @markers = @match_places.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude,
+        # to make that work : create a _infowindow partial into views/places !!!
+        infoWindow: render_to_string(partial: "infowindow", locals: { place: place })
+      }
+    end
+    ### end markers
   end
 
   def show
