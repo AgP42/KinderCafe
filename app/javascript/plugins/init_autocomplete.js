2506ce3d392f991a5a80682e56d343783d1df3bf
Agathe Pinel <agp.com@mailbox.org>
2019-07-03 13:21:23 +0200

    fix locate me on index page

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,16 +3,29 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('address');
   if (addressInput) {
-    places({ container: addressInput });
+    let placesAutocomplete = places({ container: addressInput }).configure({
+    countries: ['de']
+  });
+ placesAutocomplete.on('change', e => console.log(e.suggestion));
   }
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    // console.log(position.coords.latitude, position.coords.longitude);
-    const newURL = window.location.pathname + `?&lat=${position.coords.latitude}&long=${position.coords.longitude}`;
-    window.location = newURL;
+  let $button = document.querySelector('.ap-icon-pin');
+
+  /* If the user does a click on the Locate me button, do a reverse query */
+  $button.addEventListener('click', function(e) {
+    e.preventDefault();
+    $button.textContent = 'Searching...';
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // console.log(position.coords.latitude, position.coords.longitude);
+      const newURL = window.location.pathname + `?&lat=${position.coords.latitude}&long=${position.coords.longitude}`;
+      window.location = newURL;
+
+    });
 
   });
 
 };
 
 export { initAutocomplete };
+
